632814d1e915a4b59c60ff42fa78b2c03a536465
Andrew Heekin <andrewheekin@gmail.com>
2015-07-21 12:28:56 -0400

    added config

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
